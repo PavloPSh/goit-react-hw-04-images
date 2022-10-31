ce3a1c294ba83a1ff39d3fb718ff79b0250c02ed
PavloPSh <pavlopshe@gmail.com>
2022-10-31 22:00:00 +0200

    update

--- src/components/ImageFinder/ImageFinder.jsx
+++ src/components/ImageFinder/ImageFinder.jsx
@@ -32,7 +32,7 @@ export const ImageFinder = () => {
                 return;
             }
     
-        setLoading(true);
+            setLoading(true);
     
             try {
                 const data = await getImages(page, searchInput);
@@ -88,115 +88,15 @@ export const ImageFinder = () => {
 
     return (
         <ImageFinderBox>
-            {modalOpen && <Modal onClose={()=>{closeModal()}} src={modalSrc} id={modalId}  />}
+            {modalOpen && <Modal onClose={() => { closeModal() }} src={modalSrc} id={modalId} />}
             <Searchbar onSubmit={onSearch} />
             {loading && <Loader />}
-            {error && <p>Something went wrong...</p>} 
+            {error && <p>Something went wrong...</p>}
             {isImages && <ImageGallery images={images} onClick={openModal} />}
             {isImages && <LoadMoreButton text={'Load more'} onClick={loadMore} />}
             <ToastContainer autoClose={1500} />
         </ImageFinderBox>
-    )    
-}
+    )
+};
 
 
-// export class ImageFinder extends Component {
-//     state = {
-//         images: [],
-//         page: 1,
-//         search:'',
-//         loading: false,
-//         error: null,
-//         modalOpen: false,
-//         modalSrc: "",
-//         modalId:'',
-
-//     }
-
-//     // componentDidMount() {
-//     //     this.fetchImages()
-//     // }
-
-//     componentDidUpdate(_, prevState) {
-//         const { page, search } = this.state;
-//         if (prevState.search !== search || prevState.page !== page) {
-//             this.fetchImages();
-//         }
-//     }
-
-//     async fetchImages() {
-//         const { page, search } = this.state;
-        
-//         this.setState({
-//             loading: true,
-//         });
-    
-//         try {
-//             const data = await getImages(page, search);
-//             if (data.totalHits === 0) {
-//                 return toast.error('No image:(')
-//             }
-//             this.setState(({ images }) => {
-//                 return {
-//                     images: [...images, ...data.hits]
-//                 }
-//             })
-//         } catch (error) {
-//             this.setState({ error })
-//         } finally {
-//             this.setState({ loading: false })
-//         }
-//     }
-
-//     onSearch = (search)  => {
-//         if (search !== this.state.search) {
-//             this.setState({
-//                 search,
-//                 images: [],
-//                 page: 1,    
-//             })
-//         }
-//     }
-
-//     loadMore = () => {
-//         this.setState(({ page }) => {
-//             return {
-//                 page: page + 1,
-//             }
-//         })
-//     }
-
-//     openModal = (src,id) => {
-//         this.setState({
-//             modalOpen: true, 
-//             modalSrc: src,
-//             modalId: id,
-//         })
-//     }
-
-//     closeModal = () => {
-//         this.setState({
-//             modalOpen: false,
-//         })
-//     }
-    
-//     render() {
-
-//         const { images, loading, error, modalOpen,modalSrc,modalId } = this.state;
-//         const isImages = Boolean(images.length);
-//         const { closeModal, onSearch, openModal, loadMore } = this;
-        
-
-//         return (
-//             <ImageFinderBox>
-//                 {modalOpen && <Modal onClose={closeModal} src={modalSrc} id={modalId}  />}
-//                 <Searchbar onSubmit={onSearch} />
-//                 {loading && <Loader />}
-//                 {error && <p>Something went wrong...</p>} 
-//                 {isImages && <ImageGallery images={images} onClick={openModal} />}
-//                 {isImages && <LoadMoreButton text={'Load more'} onClick={loadMore} />}
-//                 <ToastContainer autoClose={1500} />
-//             </ImageFinderBox>
-//         )
-//     }
-// }
